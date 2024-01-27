cd0a247a1f43388b1875ef36119417f5564ea768
core <core@pop-os.localdomain>
2024-01-27 21:50:51 +0530

    dontreadthechats

--- src/main/java/com/mark/web/controllers/UserController.java
+++ src/main/java/com/mark/web/controllers/UserController.java
@@ -1,16 +1,28 @@
 package com.mark.web.controllers;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.FileHandler;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mark.web.models.FileObject;
 import com.mark.web.models.User;
 import com.mark.web.services.serviceImplementation.UserServiceImplementation;
+import com.mark.web.services.serviceImplementation.UtillityServiceImplementation;
 
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -20,8 +32,9 @@ public class UserController {
 
     private static UserServiceImplementation userServiceImpl=new UserServiceImplementation();
     // private static final Logger logger=LogManager.getLogger(); 
-    // private static final UtillityServiceImplementation utils=new UtillityServiceImplementation();
+    private static final UtillityServiceImplementation utils=new UtillityServiceImplementation();
     
+    private File recievedFile;
     
     @GetMapping("/login")
     public String sendLoginPage(Model model){
@@ -30,6 +43,46 @@ public class UserController {
         return "loginUser";
     }
     
+    @GetMapping("/uploadFile")
+    public String sendUploadFile(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("Hi");
+        System.out.println("StringBuilder: "+sb); 
+        return "uploadFile";
+    }
+
+    @PostMapping("/uploadFile")
+    public String getFile(Model model,@RequestParam("image")MultipartFile file){
+        String uploadDirectory=System.getProperty("user.dir")+"/uploads";
+        
+        Path fileNameAndPath=Paths.get(uploadDirectory,file.getOriginalFilename());
+
+        StringBuilder fileName=new StringBuilder();
+        fileName.append(file.getOriginalFilename());
+        
+        System.out.println("uploadDirectory: "+uploadDirectory);
+        System.out.println("filenameAndPath: "+fileNameAndPath);
+
+        //
+
+        try{
+            byte[] fileByte=file.getBytes();
+            String hexString=utils.byteToHex(fileByte);
+            System.out.println(hexString);
+            Files.write(fileNameAndPath,file.getBytes());
+        }catch(IOException e){
+            e.printStackTrace();
+        } 
+
+
+        model.addAttribute("msg","image added "+fileName.toString());
+        return "uploadFile";
+    }
+    
+    @GetMapping("/getFile")
+    public File sendFile(){
+        return recievedFile;
+    }
 
     @GetMapping("/register")
     public String sendRegisterPage(Model model){
