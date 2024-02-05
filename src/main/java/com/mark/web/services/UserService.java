c613116f27a854fd549d4f4c72e33e48aea12725
core <core@pop-os.localdomain>
2024-02-05 16:46:20 +0530

    no encryption for now

--- src/main/java/com/mark/web/services/UserService.java
+++ src/main/java/com/mark/web/services/UserService.java
@@ -4,11 +4,12 @@ import java.sql.SQLException;
 
 
 public interface UserService {
-   public void registerUser(String username,String email,String password)throws SQLException;
-   public boolean loginUser(String username,String password)throws SQLException;
+   public void registerUser(String username,String email,String password)throws Exception;
+   public boolean loginUser(String username,String password)throws Exception;
    // public void update(User user) throws SQLException;
    public void getUser(int id) throws SQLException;
    public void deleteUser(int id)throws SQLException;
    public boolean checkUsername(String username)throws SQLException;
-   public void reportUser(String username,int id); 
+   public void reportUser(String username,int id);
+   public void addImage(int user_id,String image_description,String imageHexString)throws SQLException; 
 }
--- src/main/java/com/mark/web/services/UtillityServices.java
+++
@@ -1,7 +0,0 @@
-package com.mark.web.services;
-
-public interface UtillityServices {
-   public String generateHashedPassword(String rawPassword);
-   public boolean validatePassword(String rawPassword,String encryptedPassword);
-   public boolean stringEmpty(String s);
-}
